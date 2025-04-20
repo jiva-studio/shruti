e939c4f96d5788dd5a4d427288d483558a0584a5
Advaita Krishna das <akdasa@jiva.studio>
2025-04-20 09:39:12 +0200

    feat(mobile): start playing when user clicks on Track (#122)

---
+++ modules/apps/mobile/src/player/composables/usePlayer.ts
@@ -0,0 +1,5 @@
+import { AudioPlayer } from '@shruti/audio-player'
+
+export function usePlayer() { 
+  return AudioPlayer
+}
---
+++ modules/libs/audioPlayer/android/src/main/java/studios/akdasa/shruti/audioplayer/mediaStateNotifications/IMediaStateNotifier.java
@@ -0,0 +1,5 @@
+package studios.akdasa.shruti.audioplayer.mediaStateNotifications;
+
+public interface IMediaStateNotifier {
+    void send(MediaState state);
+}
