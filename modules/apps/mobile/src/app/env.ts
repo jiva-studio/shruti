6d75799b6815770ccaa71cc996ab7a54c1d85b99
Advaita Krishna das <akdasa@jiva.studio>
2025-04-27 13:24:40 +0200

    feat(mobile): add telemetry (#176)

---
+++ modules/apps/mobile/src/app/env.ts
@@ -0,0 +1,8 @@
+export const ENVIRONMENT = {
+  release: 'shruti@dev',
+  dist: 'dev',
+  sentryDsn: '',
+  apiUrl: 'http://localhost:8001/',
+  databaseUrl: 'http://localhost:5984/',
+  bucketName: 'shruti-dev'
+}
