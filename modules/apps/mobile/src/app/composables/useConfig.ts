79229e5be3cf29a632abd62fdb1cf73bc72bc00e
Advaita Krishna das <akdasa@jiva.studio>
2025-04-28 08:01:11 +0000

    feat(mobile): Inject readonly auth token for production build (#181)

--- modules/apps/mobile/src/app/composables/useConfig.ts
+++ modules/apps/mobile/src/app/composables/useConfig.ts
@@ -3,22 +3,9 @@ import { ref } from 'vue'
 import { ENVIRONMENT } from '@/app/env'
 
 
-/**
- * Predefined readonly user token for API requests.
- */
-// TODO: inject another token for production build
-const READONLY_USER_TOKEN = 
-  '' +
-  '' +
-  '' +
-  '' +
-  '' +
-  '' +
-  ''
-
 export const useConfig = createGlobalState(() => {
   const appLanguage  = ref('en')
-  const authToken    = ref(READONLY_USER_TOKEN)
+  const authToken    = ref(ENVIRONMENT.readonlyAuthToken)
   const apiUrl       = ref(ENVIRONMENT.apiUrl)
   const databaseUrl  = ref(ENVIRONMENT.databaseUrl)
   const bucketName   = ref(ENVIRONMENT.bucketName)
