6222d5580deacae014fb0008b92f90b32a2c5ba0
Advaita Krishna das <akdasa@jiva.studio>
2025-05-10 17:23:34 +0000

    fix(airflow): remove vastai

--- modules/services/airflow/dags/shruti/config/__init__.py
+++ modules/services/airflow/dags/shruti/config/__init__.py
@@ -1,6 +1,5 @@
 from shruti.config.database import *
 from shruti.config.app_bucket import VAR_APP_BUCKET_GENERATE_TEMPORARY_ACCESS_KEY, VAR_APP_BUCKET_ACCESS_KEY, VAR_APP_BUCKET_NAME, AppBucketAccessKey, AppBucketGenerateTempraryAccessKey
-from shruti.config.vastai import VASTAI_ACCESS_KEY, VASTAI_PRIVATE_SSH_KEY
 from shruti.config.shruti import *
 from shruti.config.pools import *
 from shruti.config.transcripts_proofread import *
--- modules/services/airflow/dags/shruti/config/shruti.py
+++ modules/services/airflow/dags/shruti/config/shruti.py
@@ -5,19 +5,12 @@ from airflow.models import Variable
 # ---------------------------------------------------------------------------- #
 
 SHRUTI_VAKSHUDDKI_MINIMUM_AUDIOFILES = "shruti::vakshuddhi::minimim-audiofiles"
-SHRUTI_VAKSHUDDKI_VASTAI_QUERY = "shruti::vakshuddhi::vastai-query"
 BASE_URL = "shruti::base-url"
 
 
 # ---------------------------------------------------------------------------- #
 #                                    Default                                   #
 # ---------------------------------------------------------------------------- #
-
-Variable.setdefault(
-  SHRUTI_VAKSHUDDKI_VASTAI_QUERY,
-  "cuda_vers=12.4 num_gpus=1 gpu_name=RTX_4090 inet_down>=100 rentable=true geolocation in DE,BG,EE,FI,IT,MD,NO",
-  "Vast.ai query for Vakshuddhi instance"
-)
 
 Variable.setdefault(
   BASE_URL,
