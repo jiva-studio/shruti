e9c50b307cefac0bd52c7129008ac050401e822d
Advaita Krishna das <akdasa@jiva.studio>
2025-06-12 14:07:06 +0000

    feat(mobile): player progress indicator

--- modules/apps/mobile/src/pages/SettingsPage.vue
+++ modules/apps/mobile/src/pages/SettingsPage.vue
@@ -1,15 +1,24 @@
 <template>
   <Page>
-    <IonList>
-      <AppLanguageSettingsItem />
-      <SubscriptionSettingsItem />
-    </IonList>
+    <!-- App Appearance -->
+    <IonListHeader>
+      <IonLabel>{{ $t('settings.groups.appearance') }}</IonLabel>
+    </IonListHeader>
+    <AppLanguageSettingsItem />
+    <ShowPlayerProgressSettingsItem />
+
+    <!-- Pro Features -->
+    <IonListHeader>
+      <IonLabel>{{ $t('settings.groups.pro') }}</IonLabel>
+    </IonListHeader>
+    <SubscriptionSettingsItem />
   </Page>
 </template>
 
 <script setup lang="ts">
-import { IonList } from '@ionic/vue'
+import { IonListHeader, IonLabel } from '@ionic/vue'
 import { Page } from '@shruti/mobile/features/app.core'
 import { SubscriptionSettingsItem } from '@shruti/mobile/features/app.purchases'
 import { AppLanguageSettingsItem } from '@shruti/mobile/features/app.localization'
+import { ShowPlayerProgressSettingsItem } from '@shruti/mobile/features/player.progress'
 </script>
