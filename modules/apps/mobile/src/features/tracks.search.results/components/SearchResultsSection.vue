ed320a933a94b07deb1c7484ad43fb555b8dc0d1
Advaita Krishna das <akdasa@jiva.studio>
2025-06-13 11:48:10 +0000

    feat(mobile): tracks sorting

--- modules/apps/mobile/src/features/tracks.search.results/components/SearchResultsSection.vue
+++ modules/apps/mobile/src/features/tracks.search.results/components/SearchResultsSection.vue
@@ -18,10 +18,6 @@
       />
     </template>
   </TrackListItem>
-  
-  <SearchSpecifyCriteria 
-    v-if="searchResultsStore.pagesLoaded === searchResultsStore.maximumPagesToLoad"
-  />
 
   <IonInfiniteScroll
     v-if="!searchResultsStore.isLastPage"
@@ -37,7 +33,6 @@ import { IonInfiniteScroll, IonInfiniteScrollContent, InfiniteScrollCustomEvent 
 import { TrackListItem } from '@shruti/mobile/features/tracks.view'
 import { useWaiter } from '@shruti/mobile/features/app.core'
 import { useTrackSearchResultsStore } from '../composables/useTrackSearchResultsStore'
-import SearchSpecifyCriteria from './SearchSpecifyCriteria.vue'
 
 /* -------------------------------------------------------------------------- */
 /*                                Dependencies                                */
