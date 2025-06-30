a7249648cb9578f86d35ea258138f039753ce310
Advaita Krishna das <akdasa@jiva.studio>
2025-06-30 13:20:23 +0200

    feat(api): add authentication via 3d-party JWT tokens (#235)

--- modules/services/api/src/configs/auth.config.ts
+++ modules/services/api/src/configs/auth.config.ts
@@ -26,4 +26,10 @@ export default registerAs('auth', () => ({
     process.env.SHRUTI_AUTH_USER_PERMISSIONS_CACHE_TTL ?? '0',
     10,
   ),
+
+  /**
+   * Google OAuth client ID.
+   * This is used to authenticate users with Google OAuth.
+   */
+  googleOAuthClientId: process.env.SHRUTI_AUTH_GOOGLE_OAUTH_CLIENT_ID || '',
 }));
