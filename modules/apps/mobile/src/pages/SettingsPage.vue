6e7ecb9553289b0d79a73400ca4a9ea920bc1fdb
Advaita Krishna das <akdasa@jiva.studio>
2025-07-04 15:54:37 +0200

    feat(mobile): add automatic transcript opening feature (#237)

--- modules/apps/mobile/src/pages/SettingsPage.vue
+++ modules/apps/mobile/src/pages/SettingsPage.vue
@@ -15,52 +15,24 @@
     <ShowPlayerProgressSettingsItem />
     <ShowNotesTabSettingsItem />
     <HighlightCurrentSentenceSettingsItem />
+    <OpenTranscriptAutomaticallySettingsItem />
 
     <!-- Social -->
     <IonListHeader>
       <IonLabel>{{ $t('settings.groups.misc') }}</IonLabel>
     </IonListHeader>
-    <IonItem lines="none">
-      <div slot="start">
-        🔗
-      </div>
-      <IonLabel>
-        <h2>{{ $t('settings.social.socialNetworks') }}</h2>
-        <p>{{ $t('settings.social.stayConnected') }}</p>
-      </IonLabel>
-      <div slot="end">
-        <a href="https://t.me/akd_studios">
-          <IconTelegram class="icon" />
-        </a>
-        <a href="https://www.facebook.com/groups/akd.studios/">
-          <IconFacebook class="icon" />
-        </a>
-        <a href="https://vk.com/akd.studios">
-          <IconVk class="icon" />
-        </a>
-      </div>
-    </IonItem>
+    <SocialNetworksSettingsItem />
   </Page>
 </template>
 
 <script setup lang="ts">
-import { IonListHeader, IonLabel, IonItem } from '@ionic/vue'
+import { IonListHeader, IonLabel } from '@ionic/vue'
 import { Page } from '@shruti/mobile/features/app.core'
 import { SubscriptionSettingsItem } from '@shruti/mobile/features/app.purchases'
 import { AppLanguageSettingsItem } from '@shruti/mobile/features/app.localization'
 import { ShowPlayerProgressSettingsItem } from '@shruti/mobile/features/player.progress'
 import { ShowNotesTabSettingsItem } from '@shruti/mobile/features/notes'
-import { HighlightCurrentSentenceSettingsItem } from '@shruti/mobile/features/transcript'
+import { HighlightCurrentSentenceSettingsItem, OpenTranscriptAutomaticallySettingsItem } from '@shruti/mobile/features/transcript'
 import { SignInSettingsItem } from '@shruti/mobile/features/app.auth'
-import { IconTelegram } from '@shruti/mobile/features/app.ui.kit'
-import { IconFacebook } from '@shruti/mobile/features/app.ui.kit'
-import { IconVk } from '@shruti/mobile/features/app.ui.kit'
+import { SocialNetworksSettingsItem } from '@shruti/mobile/features/app.settings.contacts'
 </script>
-
-<style scoped>
-.icon {
-  width: 32px;
-  height: 32px;
-  margin: 6px;
-}
-</style>
