c2c5ecb3e4c7d63f5d7d85bd7dc2f81ced424056
Advaita Krishna das <akdasa@jiva.studio>
2025-07-20 16:08:22 +0000

    fix(auth): correct import statement for crypto module

--- modules/services/api/src/auth/services/auth.service.ts
+++ modules/services/api/src/auth/services/auth.service.ts
@@ -4,7 +4,7 @@ import { JwtService } from '@nestjs/jwt';
 import { AuthConfig, JwtConfig } from '@lectorium/api/configs';
 import { RefreshToken } from '@lectorium/protocol';
 import { v4 as uuidv4 } from 'uuid';
-import crypto from 'crypto';
+import * as crypto from 'crypto';
 
 export type Tokens = {
   accessToken: string;
