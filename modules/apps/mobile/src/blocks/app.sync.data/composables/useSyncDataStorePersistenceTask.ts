9ae9dd60a2aff89e7a653c97640e859eaf9d232e
Advaita Krishna das <akdasa@jiva.studio>
2025-07-22 09:03:20 +0000

    feat(mobile): persist last sync date

---
+++ modules/apps/mobile/src/blocks/app.sync.data/composables/useSyncDataStorePersistenceTask.ts
@@ -0,0 +1,58 @@
+import { watch, Ref, toRaw } from 'vue'
+import { storeToRefs } from 'pinia'
+import { Storage } from '@ionic/storage'
+import { useSyncDataStore } from './useSyncDataStore'
+import { useLogger } from '@shruti/mobile/core'
+
+export function useSyncDataStorePersistenceTask() {
+
+  /* -------------------------------------------------------------------------- */
+  /*                                Dependencies                                */
+  /* -------------------------------------------------------------------------- */
+
+  const store = useSyncDataStore()
+  const logger = useLogger({ module: 'app.sync.data' })
+
+  /* -------------------------------------------------------------------------- */
+  /*                                    State                                   */
+  /* -------------------------------------------------------------------------- */
+
+  const storage = new Storage({ name: 'config' })
+
+  /* -------------------------------------------------------------------------- */
+  /*                               Initialization                               */
+  /* -------------------------------------------------------------------------- */
+
+  async function start() {
+    logger.info('Starting sync persistence task...')
+    await storage.create()
+    await Promise.all([
+      bind(storeToRefs(store).lastSyncedAt, 'sync.lastSyncedAt', 0),
+    ])
+  }
+
+  /* -------------------------------------------------------------------------- */
+  /*                                   Helpers                                  */
+  /* -------------------------------------------------------------------------- */
+
+  async function bind<T>(
+    config: Ref<T>, 
+    key: string, 
+    defaultValue: T
+  ) {
+    config.value = await storage.get(key) ?? defaultValue
+    watch(config, async (value) => {
+      logger.debug(`Updating '${key}' => '${JSON.stringify(value)}'`)
+      await storage.set(key, toRaw(value))
+    }, { deep: true })
+    logger.info(`Bound '${key}' => '${JSON.stringify(config.value)}'`)
+  }
+
+  /* -------------------------------------------------------------------------- */
+  /*                                  Interface                                 */
+  /* -------------------------------------------------------------------------- */
+
+  return {
+    start
+  }
+}
--- modules/apps/mobile/src/blocks/app.sync.data/index.ts
+++ modules/apps/mobile/src/blocks/app.sync.data/index.ts
@@ -1,2 +1,3 @@
 export { useSyncData } from './composables/useSyncData'
 export { useSyncDataStore } from './composables/useSyncDataStore'
+export { useSyncDataStorePersistenceTask } from './composables/useSyncDataStorePersistenceTask'
