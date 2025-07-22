004a0e2932faeb1e20769f871d1d19f5e8b91d5d
Advaita Krishna das <akdasa@jiva.studio>
2025-07-22 14:04:13 +0000

    feat(mobile): share note

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -23,3 +23,11 @@
 # ---------------------------------------------------------------------------- #
 
 mkdir -p /workspaces/shruti/gateway/data/logs
+
+# ---------------------------------------------------------------------------- #
+#                                   CocoaPods                                  #
+# ---------------------------------------------------------------------------- #
+
+sudo apt update
+sudo apt install -y ruby-full build-essential
+sudo gem install cocoapods
--- modules/apps/mobile/src/blocks/app.notes/components/NotesList.vue
+++ modules/apps/mobile/src/blocks/app.notes/components/NotesList.vue
@@ -6,16 +6,30 @@
     :tags="note.tags"
     :author="note.trackAuthor"
     :source="note.trackTitle"
+    @click="onClick(note)"
   />
 </template>
 
 
 <script lang="ts" setup>
+import { Share } from '@capacitor/share'
 import { Note } from '../models'
 import NotesListItem from './NotesListItem.vue'
+
+/* -------------------------------------------------------------------------- */
+/*                                  Interface                                 */
+/* -------------------------------------------------------------------------- */
 
 defineProps<{
   notes: Note[]
 }>()
+
+/* -------------------------------------------------------------------------- */
+/*                                  Handlers                                  */
+/* -------------------------------------------------------------------------- */
+
+async function onClick(note: Note) {
+  await Share.share({ text: note.text })
+}
 </script>
 
