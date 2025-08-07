feb11cefb714459bc70d611977e45ac9e22055d3
Advaita Krishna das <akdasa@jiva.studio>
2025-08-07 16:22:19 +0000

    feat(mobile): place "languages" filter to the first place

--- modules/apps/mobile/src/blocks/app.tracks.search.filters/components/SearchFiltersBar.vue
+++ modules/apps/mobile/src/blocks/app.tracks.search.filters/components/SearchFiltersBar.vue
@@ -1,6 +1,30 @@
 <template>
   <SearchFilterChipsList>
     <SearchFilterChipWithListItems
+      v-model="value.languages"
+      :items="dictionary.languages"
+      :title="$t('search.filters.languages')"
+    >
+      <template #icon>
+        <svg
+          class="w-6 h-6 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m13 19 3.5-9 3.5 9m-6.125-2h5.25M3 7h7m0 0h2m-2 0c0 1.63-.793 3.926-2.239 5.655M7.5 6.818V5m.261 7.655C6.79 13.82 5.521 14.725 4 15m3.761-2.345L5 10m2.761 2.655L10.2 15"
+          />
+        </svg>
+      </template>
+    </SearchFilterChipWithListItems>
+
+    <SearchFilterChipWithListItems
       v-model="value.authors"
       :items="dictionary.authors"
       :title="$t('search.filters.authors')"
@@ -69,30 +93,6 @@
       </template>
     </SearchFilterChipWithListItems>
   
-    <SearchFilterChipWithListItems
-      v-model="value.languages"
-      :items="dictionary.languages"
-      :title="$t('search.filters.languages')"
-    >
-      <template #icon>
-        <svg
-          class="w-6 h-6 text-gray-800 dark:text-white"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="m13 19 3.5-9 3.5 9m-6.125-2h5.25M3 7h7m0 0h2m-2 0c0 1.63-.793 3.926-2.239 5.655M7.5 6.818V5m.261 7.655C6.79 13.82 5.521 14.725 4 15m3.761-2.345L5 10m2.761 2.655L10.2 15"
-          />
-        </svg>
-      </template>
-    </SearchFilterChipWithListItems>
-
     <SearchFilterChipWithListItem
       v-model="value.duration"
       :items="dictionary.durations"
