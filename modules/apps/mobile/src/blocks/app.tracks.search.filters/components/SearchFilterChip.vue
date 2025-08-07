fd1d82dfb25091a045c1febf084417e4964a7e6f
Advaita Krishna das <akdasa@jiva.studio>
2025-08-07 15:58:29 +0000

    feat(mobile): search filters appearance

--- modules/apps/mobile/src/blocks/app.tracks.search.filters/components/SearchFilterChip.vue
+++ modules/apps/mobile/src/blocks/app.tracks.search.filters/components/SearchFilterChip.vue
@@ -1,28 +1,48 @@
 <template>
-  <IonChip
+  <div
     :color="color"
     class="chip"
+    :class="{
+      'on': applied,
+      'off': !applied,
+    }"
+    size="small"
     @click="emit('click')"
   >
+    <div
+      v-if="showIcon"
+      class="icon"
+    >
+      <slot name="icon" />
+    </div>
+
     <slot />
     <IonIcon
+      v-if="showActionButton"
+      class="action"
       :icon="icon"
       @click.stop="applied ? emit('remove') : emit('click')"
     />
-  </IonChip>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
-import { IonChip, IonIcon } from '@ionic/vue'
-import { close, chevronDownOutline } from 'ionicons/icons'
+import { IonIcon } from '@ionic/vue'
+import { closeCircle, addCircle } from 'ionicons/icons'
 
 /* -------------------------------------------------------------------------- */
 /*                                  Interface                                 */
 /* -------------------------------------------------------------------------- */
 
-const { applied = false } = defineProps<{
+const { 
+  applied = false, 
+  showActionButton = true,
+  showIcon = true,
+} = defineProps<{
   applied?: boolean
+  showActionButton?: boolean
+  showIcon?: boolean
 }>()
 
 const emit = defineEmits<{
@@ -34,13 +54,53 @@ const emit = defineEmits<{
 /*                                    State                                   */
 /* -------------------------------------------------------------------------- */
 
-const icon  = computed(() => applied ? close : chevronDownOutline)
+const icon  = computed(() => applied ? closeCircle : addCircle)
 const color = computed(() => applied ? 'primary' : 'medium')
 </script>
 
 <style scoped>
 .chip {
-  margin-right: 5px;
-  margin-left: 5px;
+  padding: 6px 10px;
+  display: flex;
+  align-items: center;
+  font-size: .95rem;
+  vertical-align: middle;
+  box-sizing: border-box;
+}
+
+.ios .chip {
+  border-radius: 10px;
+}
+
+.md .chip {
+  border-radius: 4px;
+}
+
+.chip.on {
+  background-color: rgba(var(--ion-color-primary-rgb), .1);
+  color: var(--ion-color-primary);
+  border: 1px solid transparent;
+}
+
+.md .chip.on {
+  border: 1px solid rgba(var(--ion-color-primary-rgb), .1);
+}
+
+.chip.off {
+  background-color: rgba(0, 0, 0, 0.02);
+  border: 1px dashed rgba(0, 0, 0, 0.12);
+  filter: grayscale(1);
+  color: rgba(0,0,0,.5);
+}
+
+.chip .action {
+  margin-left: 8px;
+  opacity: .5;
+}
+
+.icon {
+  width: 20px;
+  height: 20px;
+  margin-right: 4px;
 }
 </style>
