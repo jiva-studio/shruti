667469b4401d572605e75f7c25d885eebff399d7
Advaita Krishna das <akdasa@jiva.studio>
2025-08-08 07:15:51 +0000

    chore(mobile): extract clock icon

--- modules/apps/mobile/src/blocks/app.tracks.search.filters/components/SearchFiltersBar.vue
+++ modules/apps/mobile/src/blocks/app.tracks.search.filters/components/SearchFiltersBar.vue
@@ -46,19 +46,7 @@
       :title="$t('search.filters.duration')"
     >
       <template #icon>
-        <svg
-          class="w-6 h-6 text-gray-800 dark:text-white"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z"
-            clip-rule="evenodd"
-          />
-        </svg>
+        <IconClock />
       </template>
     </SearchFilterChipWithListItem>
 
@@ -98,6 +86,7 @@ import IconSources from '../icons/IconSources.vue'
 import IconLocations from '../icons/IconLocations.vue'
 import IconDates from '../icons/IconDates.vue'
 import IconSort from '../icons/IconSort.vue'
+import IconClock from '../icons/IconClock.vue'
 
 /* -------------------------------------------------------------------------- */
 /*                                  Interface                                 */
