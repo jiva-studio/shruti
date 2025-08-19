76508764c955181f951ed0fe16ed765553b839c9
Advaita Krishna das <akdasa@jiva.studio>
2025-08-19 13:25:35 +0200

    feat(mobile): update fastline (#261)

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -30,4 +30,4 @@ mkdir -p /workspaces/lectorium/gateway/data/logs
 
 sudo apt update
 sudo apt install -y ruby-full build-essential ffmpeg
-sudo gem install cocoapods
+sudo gem install cocoapods bundler
