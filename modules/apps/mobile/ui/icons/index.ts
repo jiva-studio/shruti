39b873f24fe5e7046f3726bc04c579faca2d9018
Advaita Krishna das <akdasa@jiva.studio>
2026-05-13 10:34:22 +0300

    Revert "fix(mobile): tab icons — Home2 for Home, Headphones for Search"
    This reverts commit dab4665136bfc0bc535481c6279bafb3ac8bdc45.

--- modules/apps/mobile/ui/icons/index.ts
+++ modules/apps/mobile/ui/icons/index.ts
@@ -1,6 +1,6 @@
 export {
-  IconHome2Filled as IconHome,
-  IconHeadphonesFilled as IconSearch,
+  IconLayoutDashboardFilled as IconHome,
+  IconLibraryFilled as IconSearch,
   IconBookmarkFilled as IconBookmark,
   IconSettingsFilled as IconSettings,
   IconPlayerPlayFilled as IconPlay,
